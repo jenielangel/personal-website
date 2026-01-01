c928491b585e14da261d80698e58ff4f67e62e3f
jeniel <abalosjenielangel@gmail.com>
2026-01-01 11:09:46 +0800

    Fix Next.js build issue - remove CSS import from pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useEffect } from 'react'
-import styles from '../styles/styles.css'
 
 export default function Home() {
   useEffect(() => {
